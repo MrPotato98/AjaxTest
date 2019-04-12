493d14ec43cdb3e6f3a5b57ed94da9762e024543
MrPotato98 <huy.potato1998@gmail.com>
2019-04-12 19:30:06 +0700

    UploadImg

---
+++ WebApplication4/WebApplication4/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult UploadImage()
+        {
+            return View();
+        }
+        public String ProcessUpload(HttpPostedFileBase file)
+        {
+            file.SaveAs(Server.MapPath("~/Content/images/") + file.FileName);
+            return "/Content/images/" + file.FileName;
+        }
+    }
+}
